502b071ac79f92df49648c4f4ac2b245c4ba73a9
Caua Queiroz <cauaqroz@gmail.com>
2023-11-14 19:32:27 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     public static void main(String[] args) {
+        // Criando objetos das classes
+        Circulo c1 = new Circulo(5, "azul");
+        Triangulo t1 = new Triangulo(3, 4, "verde");
+        Retangulo r1 = new Retangulo(6, 8, "amarelo");
+        Quadrado q1 = new Quadrado(4, "vermelho"); // Novo objeto da classe Quadrado
+        
+        // Imprimindo as informações dos objetos
+        System.out.println(c1);
+        System.out.println(t1);
+        System.out.println(r1);
+        System.out.println(q1); // Imprimindo as informações do objeto Quadrado
     }
 }
